34c1fed2e1f91b630cb32ab74923dae0b0da6629
Ercan Erdoğan <ercanerdogan@gmail.com>
2023-02-27 13:05:02 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import * as core from "@actions/core";
+import { Backport } from "./backport";
+import { Github } from "./github";
+
+/**
+ * Called from the action.yml.
+ *
+ * Is separated from backport for testing purposes
+ */
+async function run(): Promise<void> {
+  const token = core.getInput("github_token", { required: true });
+  const pwd = core.getInput("github_workspace", { required: true });
+  const pattern = new RegExp(core.getInput("label_pattern"));
+  const description = core.getInput("pull_description");
+  const title = core.getInput("pull_title");
+  const copy_labels_pattern = core.getInput("copy_labels_pattern");
+
+  const github = new Github(token);
+  const backport = new Backport(github, {
+    pwd,
+    labels: { pattern },
+    pull: { description, title },
+    copy_labels_pattern:
+      copy_labels_pattern === "" ? undefined : new RegExp(copy_labels_pattern),
+  });
+
+  return backport.run();
+}
+
+// this would be executed on import in a test file
+run();
---
+++ src/utils.ts
@@ -0,0 +1,65 @@
+import { PullRequest } from "./github";
+
+/**
+ * @param template The template potentially containing placeholders
+ * @param main The main pull request that is backported
+ * @param target The target branchname
+ * @returns Description that can be used in the backport pull request
+ */
+export function replacePlaceholders(
+  template: string,
+  main: Pick<PullRequest, "body" | "user" | "number" | "title">,
+  target: string
+): string {
+  const issues = getMentionedIssueRefs(main.body);
+  return template
+    .replace("${pull_author}", main.user.login)
+    .replace("${pull_number}", main.number.toString())
+    .replace("${pull_title}", main.title)
+    .replace("${target_branch}", target)
+    .replace("${issue_refs}", issues.join(" "));
+}
+
+/**
+ * @param body Text in which to search for mentioned issues
+ * @returns All found mentioned issues as GitHub issue references
+ */
+export function getMentionedIssueRefs(body: string | null): string[] {
+  const issueUrls =
+    body?.match(patterns.url.global)?.map((url) => toRef(url)) ?? [];
+  const issueRefs = body?.match(patterns.ref) ?? [];
+  return issueUrls.concat(issueRefs).map((ref) => ref.trim());
+}
+
+const patterns = {
+  // matches urls to github issues at start, middle, end of line as individual word
+  // may be lead and trailed by whitespace which should be trimmed
+  // captures the `org`, `repo` and `number` of the issue
+  // https://regex101.com/r/XKRl8q/5
+  url: {
+    global:
+      /(?:^| )(?:(?:https:\/\/)?(?:www\.)?github\.com\/(?<org>[^ \/\n]+)\/(?<repo>[^ \/\n]+)\/issues\/(?<number>[0-9]+)(?:\/)?)(?: |$)/gm,
+    first:
+      /(?:^| )(?:(?:https:\/\/)?(?:www\.)?github\.com\/(?<org>[^ \/\n]+)\/(?<repo>[^ \/\n]+)\/issues\/(?<number>[0-9]+)(?:\/)?)(?: |$)/m,
+  },
+
+  // matches `#123` at start, middle, end of line as individual word
+  // may be lead and trailed by whitespace which should be trimmed
+  // captures `number` of the issue (and optionally the `org` and `repo`)
+  // https://regex101.com/r/2gAB8O/2
+  ref: /(?:^| )((?<org>[^\n #\/]+)\/(?<repo>[^\n #\/]+))?#(?<number>[0-9]+)(?: |$)/gm,
+};
+
+const toRef = (url: string) => {
+  // matchAll is not yet available to directly access the captured groups of all matches
+  // so this maps the urls to GitHub refs by matching again without the global flag
+  const result = patterns.url.first.exec(url);
+  if (!result) {
+    console.error(
+      `Expected to transform url (${url}) to GitHub reference, but it did not match pattern'`
+    );
+    return "";
+  }
+  const [, org, repo, number] = result;
+  return `${org}/${repo}#${number}`;
+};
