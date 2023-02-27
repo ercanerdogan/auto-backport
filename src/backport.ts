7dd278e7b323deda7adbb44fbc84ef24bbc8b65a
Ercan Erdoğan <ercanerdogan@gmail.com>
2023-02-27 13:05:02 +0100

    Initial commit
    Folder

---
+++ src/backport.ts
@@ -0,0 +1,393 @@
+import * as core from "@actions/core";
+import dedent from "dedent";
+
+import { CreatePullRequestResponse, PullRequest } from "./github";
+import { GithubApi } from "./github";
+import * as git from "./git";
+import * as utils from "./utils";
+
+type PRContent = {
+  title: string;
+  body: string;
+};
+
+type Config = {
+  pwd: string;
+  labels: {
+    pattern: RegExp;
+  };
+  pull: {
+    description: string;
+    title: string;
+  };
+  copy_labels_pattern?: RegExp;
+};
+
+enum Output {
+  wasSuccessful = "was_successful",
+  wasSuccessfulByTarget = "was_successful_by_target",
+}
+
+export class Backport {
+  private github;
+  private config;
+
+  constructor(github: GithubApi, config: Config) {
+    this.github = github;
+    this.config = config;
+  }
+
+  async run(): Promise<void> {
+    try {
+      const payload = this.github.getPayload();
+      const owner = this.github.getRepo().owner;
+      const repo = payload.repository?.name ?? this.github.getRepo().repo;
+      const pull_number = this.github.getPullNumber();
+      const mainpr = await this.github.getPullRequest(pull_number);
+
+      if (!(await this.github.isMerged(mainpr))) {
+        const message = "Only merged pull requests can be backported.";
+        this.github.createComment({
+          owner,
+          repo,
+          issue_number: pull_number,
+          body: message,
+        });
+        return;
+      }
+
+      const headref = mainpr.head.sha;
+      const baseref = mainpr.base.sha;
+      const labels = mainpr.labels;
+
+      console.log(
+        `Detected labels on PR: ${labels.map((label) => label.name)}`
+      );
+
+      if (!someLabelIn(labels).matches(this.config.labels.pattern)) {
+        console.log(
+          `Nothing to backport: none of the labels match the backport pattern '${this.config.labels.pattern.source}'`
+        );
+        return; // nothing left to do here
+      }
+
+      console.log(
+        `Fetching all the commits from the pull request: ${mainpr.commits + 1}`
+      );
+      await git.fetch(
+        `refs/pull/${pull_number}/head`,
+        this.config.pwd,
+        mainpr.commits + 1 // +1 in case this concerns a shallowly cloned repo
+      );
+
+      console.log(
+        "Determining first and last commit shas, so we can cherry-pick the commit range"
+      );
+
+      const commitShas = await this.github.getCommits(mainpr);
+      console.log(`Found commits: ${commitShas}`);
+
+      let labelsToCopy: string[] = [];
+      if (typeof this.config.copy_labels_pattern !== "undefined") {
+        let copyLabelsPattern: RegExp = this.config.copy_labels_pattern;
+        labelsToCopy = labels
+          .map((label) => label.name)
+          .filter(
+            (label) =>
+              label.match(copyLabelsPattern) &&
+              !label.match(this.config.labels.pattern)
+          );
+      }
+      console.log(
+        `Will copy labels matching ${this.config.copy_labels_pattern}. Found matching labels: ${labelsToCopy}`
+      );
+
+      const successByTarget = new Map<string, boolean>();
+      for (const label of labels) {
+        console.log(`Working on label ${label.name}`);
+
+        // we are looking for labels like "backport stable/0.24"
+        const match = this.config.labels.pattern.exec(label.name);
+
+        if (!match) {
+          console.log("Doesn't match expected prefix");
+          continue;
+        }
+        if (match.length < 2) {
+          console.error(
+            dedent`\`label_pattern\` '${this.config.labels.pattern.source}' \
+            matched "${label.name}", but did not capture any branchname. \
+            Please make sure to provide a regex with a capture group as \
+            \`label_pattern\`.`
+          );
+          continue;
+        }
+
+        //extract the target branch (e.g. "stable/0.24")
+        const target = match[1];
+        console.log(`Found target in label: ${target}`);
+
+        try {
+          await git.fetch(target, this.config.pwd, 1);
+        } catch (error) {
+          if (error instanceof git.GitRefNotFoundError) {
+            const message = this.composeMessageForFetchTargetFailure(error.ref);
+            console.error(message);
+            successByTarget.set(target, false);
+            await this.github.createComment({
+              owner,
+              repo,
+              issue_number: pull_number,
+              body: message,
+            });
+            continue;
+          } else {
+            throw error;
+          }
+        }
+
+        try {
+          const targetDirectory = `backport`;
+          const branchname = `${targetDirectory}/backport-${pull_number}-to-${target}`;
+
+          console.log(`Start backport to ${branchname}`);
+          try {
+            await git.checkout(branchname, `origin/${target}`, this.config.pwd);
+          } catch (error) {
+            const message = this.composeMessageForBackportScriptFailure(
+              target,
+              3,
+              baseref,
+              headref,
+              branchname
+            );
+            console.error(message);
+            successByTarget.set(target, false);
+            await this.github.createComment({
+              owner,
+              repo,
+              issue_number: pull_number,
+              body: message,
+            });
+            continue;
+          }
+
+          try {
+            await git.cherryPick(commitShas, this.config.pwd);
+          } catch (error) {
+            const message = this.composeMessageForBackportScriptFailure(
+              target,
+              4,
+              baseref,
+              headref,
+              branchname
+            );
+            console.error(message);
+            successByTarget.set(target, false);
+            await this.github.createComment({
+              owner,
+              repo,
+              issue_number: pull_number,
+              body: message,
+            });
+            continue;
+          }
+
+          console.info(`Push branch to origin`);
+          const pushExitCode = await git.push(branchname, this.config.pwd);
+          if (pushExitCode != 0) {
+            const message = this.composeMessageForGitPushFailure(
+              target,
+              pushExitCode
+            );
+            console.error(message);
+            successByTarget.set(target, false);
+            await this.github.createComment({
+              owner,
+              repo,
+              issue_number: pull_number,
+              body: message,
+            });
+            continue;
+          }
+
+          console.info(`Create PR for ${branchname}`);
+          const { title, body } = this.composePRContent(target, mainpr);
+          const new_pr_response = await this.github.createPR({
+            owner,
+            repo,
+            title,
+            body,
+            head: branchname,
+            base: target,
+            maintainer_can_modify: true,
+          });
+
+          if (new_pr_response.status != 201) {
+            console.error(JSON.stringify(new_pr_response));
+            successByTarget.set(target, false);
+            const message =
+              this.composeMessageForCreatePRFailed(new_pr_response);
+            await this.github.createComment({
+              owner,
+              repo,
+              issue_number: pull_number,
+              body: message,
+            });
+            continue;
+          }
+          const new_pr = new_pr_response.data;
+
+          if (labelsToCopy.length > 0) {
+            const label_response = await this.github.labelPR(
+              new_pr.number,
+              labelsToCopy
+            );
+            if (label_response.status != 200) {
+              console.error(JSON.stringify(label_response));
+              // The PR was still created so let's still comment on the original.
+            }
+          }
+
+          const message = this.composeMessageForSuccess(new_pr.number, target);
+          successByTarget.set(target, true);
+          await this.github.createComment({
+            owner,
+            repo,
+            issue_number: pull_number,
+            body: message,
+          });
+        } catch (error) {
+          if (error instanceof Error) {
+            console.error(error.message);
+            successByTarget.set(target, false);
+            await this.github.createComment({
+              owner,
+              repo,
+              issue_number: pull_number,
+              body: error.message,
+            });
+          } else {
+            throw error;
+          }
+        }
+      }
+
+      this.createOutput(successByTarget);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+        core.setFailed(error.message);
+      } else {
+        console.error(`An unexpected error occurred: ${JSON.stringify(error)}`);
+        core.setFailed(
+          "An unexpected error occured. Please check the logs for details"
+        );
+      }
+    }
+  }
+
+  private composePRContent(target: string, main: PullRequest): PRContent {
+    const title = utils.replacePlaceholders(
+      this.config.pull.title,
+      main,
+      target
+    );
+    const body = utils.replacePlaceholders(
+      this.config.pull.description,
+      main,
+      target
+    );
+    return { title, body };
+  }
+
+  private composeMessageForFetchTargetFailure(target: string) {
+    return dedent`Backport failed for \`${target}\`: couldn't find remote ref \`${target}\`.
+                  Please ensure that this Github repo has a branch named \`${target}\`.`;
+  }
+
+  private composeMessageForBackportScriptFailure(
+    target: string,
+    exitcode: number,
+    baseref: string,
+    headref: string,
+    branchname: string
+  ): string {
+    const reasons: { [key: number]: string } = {
+      1: "due to an unknown script error",
+      2: "because it was unable to create/access the git worktree directory",
+      3: "because it was unable to create a new branch",
+      4: "because it was unable to cherry-pick the commit(s)",
+      5: "because 1 or more of the commits are not available",
+      6: "because 1 or more of the commits are not available",
+    };
+    const reason = reasons[exitcode] ?? "due to an unknown script error";
+
+    const suggestion =
+      exitcode <= 4
+        ? dedent`\`\`\`bash
+                git fetch origin ${target}
+                git worktree add -d .worktree/${branchname} origin/${target}
+                cd .worktree/${branchname}
+                git checkout -b ${branchname}
+                ancref=$(git merge-base ${baseref} ${headref})
+                git cherry-pick -x $ancref..${headref}
+                \`\`\``
+        : dedent`Note that rebase and squash merges are not supported at this time.
+                For more information see https://github.com/korthout/backport-action/issues/46.`;
+
+    return dedent`Backport failed for \`${target}\`, ${reason}.
+
+                  Please cherry-pick the changes locally.
+                  ${suggestion}`;
+  }
+
+  private composeMessageForGitPushFailure(
+    target: string,
+    exitcode: number
+  ): string {
+    //TODO better error messages depending on exit code
+    return dedent`Git push to origin failed for ${target} with exitcode ${exitcode}`;
+  }
+
+  private composeMessageForCreatePRFailed(
+    response: CreatePullRequestResponse
+  ): string {
+    return dedent`Backport branch created but failed to create PR. 
+                Request to create PR rejected with status ${response.status}.
+
+                (see action log for full response)`;
+  }
+
+  private composeMessageForSuccess(pr_number: number, target: string) {
+    return dedent`Successfully created backport PR for \`${target}\`:
+                  - #${pr_number}`;
+  }
+
+  private createOutput(successByTarget: Map<string, boolean>) {
+    const anyTargetFailed = Array.from(successByTarget.values()).includes(
+      false
+    );
+    core.setOutput(Output.wasSuccessful, !anyTargetFailed);
+
+    const byTargetOutput = Array.from(successByTarget.entries()).reduce<string>(
+      (i, [target, result]) => `${i}${target}=${result}\n`,
+      ""
+    );
+    core.setOutput(Output.wasSuccessfulByTarget, byTargetOutput);
+  }
+}
+
+/**
+ * Helper method for label arrays to check that it matches a particular pattern
+ *
+ * @param labels an array of labels
+ * @returns a 'curried' function to easily test for a matching a label
+ */
+function someLabelIn(labels: { name: string }[]): {
+  matches: (pattern: RegExp) => boolean;
+} {
+  return {
+    matches: (pattern) => labels.some((l) => pattern.test(l.name)),
+  };
+}
---
+++ src/test/utils.test.ts
@@ -0,0 +1,255 @@
+import dedent from "dedent";
+import { getMentionedIssueRefs, replacePlaceholders } from "../utils";
+
+describe("get mentioned issues", () => {
+  describe("returns an empty list", () => {
+    it("for an null text", () => {
+      expect(getMentionedIssueRefs(null)).toHaveLength(0);
+    });
+
+    it("for an empty text", () => {
+      expect(getMentionedIssueRefs("")).toHaveLength(0);
+    });
+
+    it("for a text without mentioned issues", () => {
+      expect(getMentionedIssueRefs(text({}))).toHaveLength(0);
+    });
+
+    it("for a text with an issue reference as part of a word", () => {
+      expect(getMentionedIssueRefs(text({ part: "#123" }))).toHaveLength(0);
+    });
+
+    it("for a text with an external issue reference as part of a word", () => {
+      expect(
+        getMentionedIssueRefs(text({ part: "zeebe-io/zeebe#123" }))
+      ).toHaveLength(0);
+    });
+
+    it("for a text with an issue url as part of a word", () => {
+      expect(
+        getMentionedIssueRefs(
+          text({ part: "github.com/zeebe-io/backport-action/issues/123" })
+        )
+      ).toHaveLength(0);
+    });
+  });
+
+  describe("returns a single reference", () => {
+    it("for a text with an issue reference at the start", () => {
+      expect(getMentionedIssueRefs(text({ start: "#123" }))).toEqual(["#123"]);
+    });
+    it("for a text with an issue reference in the middle", () => {
+      expect(getMentionedIssueRefs(text({ middle: "#123" }))).toEqual(["#123"]);
+    });
+    it("for a text with an issue reference at the end", () => {
+      expect(getMentionedIssueRefs(text({ end: "#123" }))).toEqual(["#123"]);
+    });
+
+    it("for a text with an external issue reference at the start", () => {
+      expect(
+        getMentionedIssueRefs(text({ start: "zeebe-io/zeebe#123" }))
+      ).toEqual(["zeebe-io/zeebe#123"]);
+    });
+    it("for a text with an external issue reference in the middle", () => {
+      expect(
+        getMentionedIssueRefs(text({ middle: "zeebe-io/zeebe#123" }))
+      ).toEqual(["zeebe-io/zeebe#123"]);
+    });
+    it("for a text with an external issue reference at the end", () => {
+      expect(
+        getMentionedIssueRefs(text({ end: "zeebe-io/zeebe#123" }))
+      ).toEqual(["zeebe-io/zeebe#123"]);
+    });
+
+    it("for a text with an issue url at the start", () => {
+      expect(
+        getMentionedIssueRefs(
+          text({ start: "github.com/zeebe-io/backport-action/issues/123/" })
+        )
+      ).toEqual(["zeebe-io/backport-action#123"]);
+    });
+    it("for a text with an issue url in the middle", () => {
+      expect(
+        getMentionedIssueRefs(
+          text({ middle: "github.com/zeebe-io/backport-action/issues/123" })
+        )
+      ).toEqual(["zeebe-io/backport-action#123"]);
+    });
+    it("for a text with an issue url at the end", () => {
+      expect(
+        getMentionedIssueRefs(
+          text({ end: "github.com/zeebe-io/backport-action/issues/123" })
+        )
+      ).toEqual(["zeebe-io/backport-action#123"]);
+    });
+  });
+
+  describe("returns all references", () => {
+    it("for a text with an issue reference at the start, middle and end", () => {
+      expect(
+        getMentionedIssueRefs(
+          text({ start: "#123", middle: "#234", end: "#345" })
+        )
+      ).toEqual(["#123", "#234", "#345"]);
+    });
+    it("for a text with an external issue reference at the start, middle and end", () => {
+      expect(
+        getMentionedIssueRefs(
+          text({
+            start: "zeebe-io/zeebe#123",
+            middle: "zeebe-io/zeebe#234",
+            end: "zeebe-io/zeebe#345",
+          })
+        )
+      ).toEqual([
+        "zeebe-io/zeebe#123",
+        "zeebe-io/zeebe#234",
+        "zeebe-io/zeebe#345",
+      ]);
+    });
+    it("for a text with an issue url at the start, middle and end", () => {
+      const base = "github.com/zeebe-io/backport-action/issues/";
+      expect(
+        getMentionedIssueRefs(
+          text({ start: `${base}123`, middle: `${base}234`, end: `${base}345` })
+        )
+      ).toEqual([
+        "zeebe-io/backport-action#123",
+        "zeebe-io/backport-action#234",
+        "zeebe-io/backport-action#345",
+      ]);
+    });
+    it("for a text with an external issue url at the start, middle and end", () => {
+      const base = "github.com/zeebe-io/zeebe/issues/";
+      expect(
+        getMentionedIssueRefs(
+          text({ start: `${base}123`, middle: `${base}234`, end: `${base}345` })
+        )
+      ).toEqual([
+        "zeebe-io/zeebe#123",
+        "zeebe-io/zeebe#234",
+        "zeebe-io/zeebe#345",
+      ]);
+    });
+  });
+});
+
+describe("compose body/title", () => {
+  const main_default = {
+    number: 123,
+    body: "foo-body",
+    user: { login: "foo-author" },
+    title: "some pr title",
+  };
+  const target = "foo-target";
+
+  describe("returns same value as provided template", () => {
+    it("for an empty template", () => {
+      expect(replacePlaceholders("", main_default, target)).toEqual("");
+    });
+
+    it("for a template without placeholders", () => {
+      const template = text({});
+      expect(replacePlaceholders(template, main_default, target)).toEqual(
+        template
+      );
+    });
+
+    it("for a template with unknown placeholders", () => {
+      const template = text({
+        start: "${abc}",
+        middle: "${def}",
+        end: "${ghi}",
+        part: "${jkl}",
+      });
+      expect(replacePlaceholders(template, main_default, target)).toEqual(
+        template
+      );
+    });
+  });
+
+  describe("returns evaluated templated", () => {
+    it("for a template with target_branch placeholder", () => {
+      const template = "Backport of some-title to `${target_branch}`";
+      expect(replacePlaceholders(template, main_default, target)).toEqual(
+        "Backport of some-title to `foo-target`"
+      );
+    });
+
+    it("for a template with pull_number placeholder", () => {
+      const template = "Backport of #${pull_number} to some-target";
+      expect(replacePlaceholders(template, main_default, target)).toEqual(
+        "Backport of #123 to some-target"
+      );
+    });
+
+    it("for a template with pull_title placeholder", () => {
+      const template = "Backport of ${pull_title} to some-target";
+      expect(replacePlaceholders(template, main_default, target)).toEqual(
+        "Backport of some pr title to some-target"
+      );
+    });
+
+    describe("for a template with issue_refs placeholder", () => {
+      const template = "Backport that refers to: ${issue_refs}";
+
+      it("and body has no referred issues", () => {
+        expect(replacePlaceholders(template, main_default, target)).toEqual(
+          "Backport that refers to: "
+        );
+      });
+
+      it("and body has a referred issue", () => {
+        expect(
+          replacePlaceholders(
+            template,
+            {
+              ...main_default,
+              body: "Body mentions #123 and that's it.",
+            },
+            target
+          )
+        ).toEqual("Backport that refers to: #123");
+      });
+
+      it("and body has some referred issues", () => {
+        expect(
+          replacePlaceholders(
+            template,
+            {
+              ...main_default,
+              body: "This body refers to #123 and foo/bar#456",
+            },
+            target
+          )
+        ).toEqual("Backport that refers to: #123 foo/bar#456");
+      });
+    });
+
+    it("for a template with pull_author placeholder", () => {
+      const template = "Backport of pull made by @${pull_author}";
+      expect(replacePlaceholders(template, main_default, target)).toEqual(
+        "Backport of pull made by @foo-author"
+      );
+    });
+  });
+});
+
+function text({
+  start = "",
+  middle = "",
+  end = "",
+  part = "",
+}: {
+  start?: string;
+  middle?: string;
+  end?: string;
+  part?: string;
+}) {
+  return dedent`${start ?? ""} foo bar
+                bar bar ${middle ?? ""} bar 
+
+                foo/${part ?? ""} foo${part ?? ""}foo ${part ?? ""}foo
+                
+                foo bar bar foo ${end ?? ""}`;
+}
